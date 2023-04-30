214cd491fd5ec0d9f90f71c4655567a0d9db150d
crictoid <93058533+crictoid@users.noreply.github.com>
2023-04-30 15:00:56 -0400

    Add files via upload

---
+++ BulmerRectangle.cpp
@@ -0,0 +1,53 @@
+# Mark Bulmer CSC-234 - 1-21-2022
+# Rectangle class 
+
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+ // Data members
+private:
+ double width, height;
+public:
+ Rectangle()
+ {
+ width = height = 1;
+ }
+ // Constructor
+ Rectangle(double newWidth, double newHeight)
+ {
+ width = newWidth;
+ height = newHeight;
+ }
+ double getWidth()
+ {
+ return width;
+ }
+ double getHeight()
+ {
+ return height;
+ }
+ double getArea()
+ {
+ return width * height;
+ }
+ double getPerimeter()
+ {
+ return 2 * (width+height);
+ }
+};
+int main() {
+ Rectangle myRectangle(4, 40);
+ cout << "The area of a rectangle with width " <<
+ myRectangle.getWidth() << " and height " <<
+ myRectangle.getHeight() << " is " <<
+ myRectangle.getArea() << " and perimeter is " <<
+ myRectangle.getPerimeter() << endl;
+ Rectangle yourRectangle(3.5, 35.9);
+ cout << "The area of a rectangle with width " <<
+ yourRectangle.getWidth() << " and height " <<
+ yourRectangle.getHeight() << " is " <<
+ yourRectangle.getArea() << " and perimeter is " <<
+ yourRectangle.getPerimeter() << endl;
+ return 0;
+} 
